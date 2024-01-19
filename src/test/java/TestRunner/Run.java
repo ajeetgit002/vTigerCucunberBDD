18607b087638e98e891389010da7a30f1ace0e49
ajit <ajeetyadav8092@gmail.com>
2024-01-19 21:42:47 +0530

    This is a common midification

--- src/test/java/TestRunner/Run.java
+++ src/test/java/TestRunner/Run.java
@@ -5,20 +5,14 @@ import io.cucumber.testng.CucumberOptions;
 
 //@RunWith(Cucumber.class)
 @CucumberOptions(
-		features =".//Features/", ///      Run single feature 
-				//features =".//Features/",            // Run All Feature 
-				//features=   {".//Features/CreateAc.feature",".//Features/LoginFeature.feature"}, // Run Specific Features 
+		
+				features=   {".//Features/CreateAc.feature",".//Features/LoginFeature.feature"}, // Run Specific Features 
 		glue="stepDefinition",
 		dryRun = false,
 		
-		//tags="@regression",        // Run Only @ regression run
-			//	tags= "@Sanity or @regression",        //Run that feature which have one feature @Sanity or @regression 
-				//tags= "@Sanity and @regression",       // Run That feature Which Both feature  @Sanity and @regression
-				//tags= "@Sanity and not  @regression", //Skip @regression  feature
-		//tags="@Sanity",
 		monochrome = true,
 				plugin={"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
-				//plugin = {"pretty","html:target/Reports/reports.html"}
+				
 		
 		
 		)
